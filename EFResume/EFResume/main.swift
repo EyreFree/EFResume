1298367572135606f0a4e991f23e44c859ba1c99
EyreFree <zheng.xu03@ele.me>
2017-09-06 14:12:15 +0800

    Setup project

---
+++ EFResume/EFResume/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EFResume
+//
+//  Created by EyreFree on 2017/9/6.
+//  Copyright © 2017年 eyrefree. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
